d3240b08a022677a07704102342d4855a80c0cfd
Kegan Maher <kegan.maher@gmail.com>
2025-10-02 23:38:01 -0700

    feat: AI picks highest total value move

--- src/app/ai/index.js
+++ src/app/ai/index.js
@@ -1,5 +1,6 @@
 export { AI } from "./base.js";
 export { HighestCardAI } from "./highest_card.js";
+export { HighestValueAI } from "./highest_value.js";
 export { LowestCardAI } from "./lowest_card.js";
 export { LowestValueAI } from "./lowest_value.js";
 export { PassAI } from "./pass.js";
